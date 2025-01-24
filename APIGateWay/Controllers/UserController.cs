4c5ba3f4ddbe7b967cd1b8794f0da1db0dfab2f0
Mueed Hassan <mueedhassan640@gmail.com>
2025-01-24 10:59:45 +0500

    Add project files.

---
+++ APIGateWay/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using APIGateWay.Data;
+using APIGateWay.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIGateWay.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")] //Get API Users
+    public class UserController : ControllerBase
+    {
+        private readonly DataContextClass _context;
+
+        public UserController(DataContextClass context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<App_User>>> GetUsers()
+        {
+            var users =await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<App_User>> GetUser(int id) 
+        {
+            var user =await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
---
+++ APIGateWay/Data/DataContextClass.cs
@@ -0,0 +1,16 @@
+﻿using APIGateWay.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIGateWay.Data
+{
+    public class DataContextClass:DbContext
+    {
+        public DataContextClass(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+        public DbSet<App_User> Users { get; set; }
+    }
+    
+}

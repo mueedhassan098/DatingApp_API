d2890eb28fd37731675d1bea9a67580fe7fe8267
Mueed Hassan <mueedhassan640@gmail.com>
2025-05-13 23:21:22 +0500

    End of Section 11

--- APIGateWay/Dtos/RegisterDto.cs
+++ APIGateWay/Dtos/RegisterDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace APIGateWay.Dtos
 {
@@ -7,7 +8,18 @@ namespace APIGateWay.Dtos
         [Required]
         public string Username { get; set; }
         [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateOnly? DateOfBirth { get; set; } //optional to make  required work!
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
         [StringLength(8,MinimumLength =4)]
-        public string Password { get; set; }
+        [JsonPropertyName("password")]
+        public string Code { get; set; }
     }
 }
